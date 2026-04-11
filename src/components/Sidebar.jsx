c24ccabd25843e1552b36937da521ad562009c3d
Imesh Munasinghe <imesh5lk@gmail.com>
2026-04-11 23:24:05 +0530

    Added the supplier and seller registration tab to the side bar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,6 +13,7 @@ import {
   MdLogout,
   MdChevronLeft,
   MdChevronRight,
+  MdPersonAdd,
 } from "react-icons/md";
 
 export default function Sidebar() {
@@ -29,6 +30,8 @@ export default function Sidebar() {
     { id: 7, icon: <MdBarChart size={22} />, text: "Analytics", path: "/admin/analytics" },
     { id: 8, icon: <MdNotifications size={22} />, text: "Notifications", path: "/admin/notifications" },
     { id: 9, icon: <MdPayment size={22} />, text: "Admin Payments", path: "/admin/adminPayments" },
+    { id: 10, icon: <MdPersonAdd size={22} />, text: "Register User", path: "/admin/register-user" },
+    //{ id: 10, icon: <MdPersonAdd size={22} />, text: "Registration", path: "/admin/register-user" },
   ];
 
   const handleLogout = () => {
