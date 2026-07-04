f187bd6a6db922ae313fc65688ee34434a060f6e
gayangi <gayangidevindi@gmail.com>
2026-07-04 10:08:08 +0530

    edit the app.jsx , with rename the AIAnalytics

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ import UpdateDelivery from "./pages/supplier/UpdateDelivery";
 import Settings from "./pages/supplier/Settings";
 import InvoicePayments from "./pages/supplier/InvoicePayments";
 import RestockAlert from "./pages/supplier/RestockAlert";
-import AIAnalytics from "./pages/supplier/AIAnalytics";
+import AIAnalytics from "./pages/supplier/AiAnalytics";
 
 /* ADMIN */
 import AdminDashboard from "./pages/admin/AdminDashboard";
