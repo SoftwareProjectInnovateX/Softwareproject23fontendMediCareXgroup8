f428bbcff8d1a691fdcf49b301771115fdc36e5e
gayangi <gayangidevindi@gmail.com>
2026-07-09 08:14:03 +0530

    add the url of backend for the localhost parts

--- src/pages/customer/HomePage.jsx
+++ src/pages/customer/HomePage.jsx
@@ -29,7 +29,7 @@ export default function HomePage() {
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
-        const response = await fetch(`http://localhost:5000/api/customer/blogs/latest`);
+        const response = await fetch(`https://backendg08innovatex-production.up.railway.app/api/customer/blogs/latest`);
         if (!response.ok) throw new Error(`Server error: ${response.status}`);
         const data = await response.json();
         setBlogPosts(Array.isArray(data) ? data : []);
